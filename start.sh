635ffc36f66d94307e867d41cd33b519c8b5556f
root <root@leom.localdomain>
2026-04-12 08:49:02 +0800

    add

--- start.sh
+++ start.sh
@@ -5,18 +5,18 @@ CUDA_VISIBLE_DEVICES=0
 #--use-flash-attention
 #--use-sage-attention
 python main.py \
+	--cuda-device $CUDA_VISIBLE_DEVICES \
+	--port 9901 \
+	--listen 0.0.0.0 \
 	--enable-manager \
 	--enable-cors-header \
-	--listen 0.0.0.0 \
-	--port 9901 \
-	--cuda-device $CUDA_VISIBLE_DEVICES \
 	--force-fp16 \
 	--fp8_e4m3fn-unet \
 	--bf16-vae \
 	--fp8_e4m3fn-text-enc \
 	--supports-fp8-compute \
-	--cache-lru 100 \
+	--cache-lru 32 \
 	--normalvram \
 	--fast \
-	--use-flash-attention \
+	--use-sage-attention \
 	--mmap-torch-files 
